07d30eefde3afbfa44a8b36ce5f7dbba00a20a40
yugandhar <boyayuggandhar135@gmail.com>
2025-09-20 08:57:55 +0530

    home page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Component } from "react";
+import "./index.css"
+
+class App extends Component{
+  render(){
+    return(
+      <div className="app-container">
+        <h1>Yugandhar</h1>
+      </div>
+    )
+  }
+}
+export default App
